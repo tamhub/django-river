27585fe1eb00f30a0cd63cce76b633de2ce57845
mohamedAbdElhameed <mohamedabdelhameed34@gmail.com>
2024-06-12 13:13:01 +0300

    feat(river): Update Workflow model with new fields [FMS-2349]
    The Workflow model has been enhanced with the addition of 'title' and 'description' fields. Code formatting has also been improved for better readability. This change provides more flexibility and detail for each workflow instance.

--- river/models/workflow.py
+++ river/models/workflow.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models import PROTECT
+
 try:
     # Try to import gettext_lazy for Django 3.0 and newer
     from django.utils.translation import gettext_lazy as _
@@ -14,16 +15,26 @@ from river.models.managers.workflowmetada import WorkflowManager
 
 class Workflow(BaseModel):
     class Meta:
-        app_label = 'river'
+        app_label = "river"
         verbose_name = _("Workflow")
         verbose_name_plural = _("Workflows")
         unique_together = [("content_type", "field_name")]
 
     objects = WorkflowManager()
-
-    content_type = models.ForeignKey(app_config.CONTENT_TYPE_CLASS, verbose_name=_('Content Type'), on_delete=PROTECT)
+    title = models.CharField(_("Title"), max_length=100, null=True, blank=True)
+    description = models.CharField(
+        _("Description"), max_length=200, null=True, blank=True
+    )
+    content_type = models.ForeignKey(
+        app_config.CONTENT_TYPE_CLASS, verbose_name=_("Content Type"), on_delete=PROTECT
+    )
     field_name = models.CharField(_("Field Name"), max_length=200)
-    initial_state = models.ForeignKey(State, verbose_name=_("Initial State"), related_name='workflow_this_set_as_initial_state', on_delete=PROTECT)
+    initial_state = models.ForeignKey(
+        State,
+        verbose_name=_("Initial State"),
+        related_name="workflow_this_set_as_initial_state",
+        on_delete=PROTECT,
+    )
 
     def natural_key(self):
         return self.content_type, self.field_name
